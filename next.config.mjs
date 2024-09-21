c6ddcd5c18fb6f160b235ef6ea9b8eeb1638c03f
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-09-21 17:08:35 +0200

    Added carlosnunezmx/animeflv

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www3.animeflv.net',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
