6b91cf474410f07076f3b4763848cbbd96c809e7
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-09-28 20:13:08 +0200

    Fix sEARCH PAGE

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,15 +1,21 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
+import SearchAnime from "@/app/components/ui/searchAnime";
 
 export default function Page() {
-    const router = useRouter();
-
-    router.push("/");
 
     return (
         <>
+            <section className="w-full py-12 md:py-14 lg:py-12 bg-white dark:bg-gray-900 flex flex-col items-center">
+                <div className="w-full max-w-sm space-y-2">
+                    <SearchAnime searhValue=""></SearchAnime>
+                </div>
+
+            </section>
+
+            <div className="p-5">
+                <p className="w-full text-center text-2xl">Busca algo</p>
+            </div>
         </>
+
+
     )
 }
